4bfe5cd566dc84e82dbedcecaaf0470847fcb6cd
Naga Akhil Belide <akhil@ssrb-vpn2-15-40.vpn.ufl.edu>
2023-11-27 18:49:41 -0500

    header thoda done and filters and grapgh side by side

--- dbms-frontend/src/pages/Header.jsx
+++ dbms-frontend/src/pages/Header.jsx
@@ -9,12 +9,12 @@ function Header(){
 
     return(
         <>
-            <div className='shadow-md w-full fixed top-0 left-0'>
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
+            <div className='absolute top-0 left-0 bg-blue-500 w-100 h-15'>
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full my-3 md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
                     {
                         Links.map((link) => (
                         <li className='md:ml-8 md:my-0 my-7 font-semibold'>
-                            <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
+                            <a href={link.link} className='text-white hover:text-blue-400 duration-500 no-underline  active:text-black'>{link.name}</a>
                         </li>))
                     }
                 </ul>
--- dbms-frontend/src/pages/Query1.jsx
+++ dbms-frontend/src/pages/Query1.jsx
@@ -54,43 +54,55 @@ function Query1(){
 
 
     return (
-        <div>
-        <Header/>
-        <Container>
-        <div>
-            <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Enter names..."
-            />
-            {results && results.length > 0 && 
-                results.map((result) => (
-                    <div
-                      key={result.id}
-                      className={`result-container ${selectedResult === result ? 'selected' : ''}`}
-                      onClick={() => handleResultClick(result)}
-                    >
-                      <p>{result.name}</p>
+        <div className="">
+            <Header/> 
+            <br/>
+            <div className="flex mt-20 align-top">
+                <div className="flex-none ml-10 mr-5 pr-5 border-r">
+                    <label className="block mb-2 text-sm font-medium text-black-900">Search Player</label>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        className="bg-grey-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                        placeholder=""
+                    />
+                    
+
+                    <div className="border border-gray-300">
+                    {results && results.length > 0 && 
+                        results.map((result) => (
+                            
+                            <div 
+                            key={result.id}
+                            className="p-2 text-center hover:bg-indigo-500 hover:text-white"
+                           // className={`result-container ${selectedResult === result ? 'selected' : ''}`}
+                            onClick={() => handleResultClick(result)}
+                            >
+                            <p className="block mb-2 text-sm font-medium text-black-900">{result.name}</p>
+                            </div>
+                           
+                        ))
+                    }
                     </div>
-                  ))
-            }
-        </div>
-       
-            {selectedResult && (
-                <div>
-                <p>Selected Name: {selectedResult.name}</p>
+
                 </div>
-            )}
-      
-        
-            {playerDetails && playerDetails.length>0 && (
-                <Query1Graph 
-                data = {playerDetails}
-                />
-            )}
-            
-        </Container>
+                
+                <div className="flex-none">
+                    {selectedResult && (
+                        <div>
+                        <p className="block mb-2 text-sm font-medium text-black-900 text-center">Selected Name: {selectedResult.name}</p>
+                        </div>
+                    )}
+                    <br/>
+                    {playerDetails && playerDetails.length>0 && (
+                        <Query1Graph 
+                        data = {playerDetails}
+                        />
+                    )}
+                </div>
+                
+            </div>
         </div>
     );
 }
